0cecf8a80ddb68fb31502005c6cf3df2943deba8
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2022-10-07 20:48:05 +0530

    Time: 15 ms (27.45%), Space: 10.7 MB (20.44%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        int s1 = nums1.size();
-        int s2 = nums2.size();
-        unordered_map<int,int> m1,m2;
-        for(auto it:nums1){
-            m1[it]++;
+map<int,int>freq;
+        vector<int>ans;
+        for(int i = 0;i<nums1.size();i++){
+            freq[nums1[i]]++;
         }
-        
-        for(auto it:nums2){
-            m2[it]++;
-        }
-        if(s1<s2){
-            //we will check for nums1's element for every m2----
-            for(auto i:nums1){
-                if(m2[i]){
-                    ans.push_back(i);
-                    m2[i]--;
-                }
-            }
-        }else{
-            //we will check for nums2's element for every m1----
-            for(auto i:nums2){
-                if(m1[i]){
-                    ans.push_back(i);
-                    m1[i]--;
-                }
+        for(int i = 0;i<nums2.size();i++){
+            if (freq[nums2[i]] > 0){
+                freq[nums2[i]]--; 
+                ans.push_back(nums2[i]);
             }
         }
-        
         return ans;
     }
 };
