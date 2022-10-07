0bddcdd74fec10224f9f33b4200d708050810622
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2022-10-07 20:06:09 +0530

    Time: 17 ms (80.15%), Space: 12 MB (14.00%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int val = target - nums[i];
+            if(m.count(val)){
+                ans.push_back(m[val]);
+                ans.push_back(i);
+            }else{
+                m[nums[i]] = i;
+            }
+        }
+        
+        return ans;
+    }
+};
